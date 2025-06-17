a809e6d8eb59de263d3fe886f59d31767fd24e84
Joserra13 <Joserra_013@hotmail.com>
2025-06-17 13:51:34 +0200

    feat(): Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+module.exports = app; // Export the app for testing purposes
