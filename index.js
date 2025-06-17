04230c0d3044f1b0a94bf2cec75f6f9d59182877
Joserra13 <Joserra_013@hotmail.com>
2025-06-17 13:56:31 +0200

    feat(): change msg in main

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.status(200).json("Hello World!");
+  res.status(200).json("Hello World!" + new Date().toISOString());
 });
 
 app.listen(port, () => {
