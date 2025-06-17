c11077ba185a89b7f89499adbbd4b0c70518b9c6
Joserra13 <Joserra_013@hotmail.com>
2025-06-18 00:50:48 +0200

    fix(): return live data

--- index.js
+++ index.js
@@ -7,29 +7,32 @@ const { db } = require("./firebaseAdmin");
 const app = express();
 const port = 3000;
 
+let data;
+
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 app.use(express.json());
 
-async function fetchFishFeederData() {
-  try {
-    const fishFeederCollection = db.collection("fishFeeder");
-    const snapshot = await fishFeederCollection.get();
-    const data = [];
-    snapshot.forEach((doc) => {
-      data.push({ id: doc.id, ...doc.data() });
-    });
-    return data;
-  } catch (error) {
-    console.error("Error fetching fish feeder data:", error);
-    throw error;
-  }
-}
+// async function fetchFishFeederData() {
+//   try {
+//     const fishFeederCollection = db.collection("fishFeeder");
+//     const snapshot = await fishFeederCollection.get();
+//     const data = [];
+//     snapshot.forEach((doc) => {
+//       data.push({ id: doc.id, ...doc.data() });
+//     });
+//     return data;
+//   } catch (error) {
+//     console.error("Error fetching fish feeder data:", error);
+//     throw error;
+//   }
+// }
 
 const doc = db.collection('fishFeeder')
 
 const observer = doc.onSnapshot(docSnapshot => {
   console.log("📦 Collection snapshot received.");
   docSnapshot.forEach(doc => {
+    data = doc.data();
     console.log(`${doc.id} =>`, doc.data());
   });
 
@@ -38,15 +41,17 @@ const observer = doc.onSnapshot(docSnapshot => {
 });
 
 app.get("/", (req, res) => {
-  fetchFishFeederData()
-    .then((data) => {
-      console.log("Fish Feeder Data:", data);
-      res.status(200).json(data);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      res.status(500).json({ error: 'Failed to fetch data' });
-    });
+  // fetchFishFeederData()
+  //   .then((data) => {
+  //     console.log("Fish Feeder Data:", data);
+  //     res.status(200).json(data);
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error:", error);
+  //     res.status(500).json({ error: 'Failed to fetch data' });
+  //   });
+
+  res.status(200).json(data);
 });
 
 app.listen(port, () => {
