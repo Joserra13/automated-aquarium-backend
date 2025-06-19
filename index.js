8ed78a01e5fb669e04ee4ec78450ca92f3b97a71
Joserra13 <Joserra_013@hotmail.com>
2025-06-19 19:14:58 +0200

    fix(): Unneded bracket

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 const express = require("express");
 const favicon = require("serve-favicon");
 const path = require("path");
-const apiKeyAuth = require('./middleware/apiKeyAuth');
+const apiKeyAuth = require("./middleware/apiKeyAuth");
 const { db } = require("./firebaseAdmin");
 
 const app = express();
 const port = 3000;
 
+let fishFeederData = {
+  schedule0Enabled: true,
+  feednow: false,
+  schedule2Enabled: false,
+  schedule2: "00:00",
+  schedule1Enabled: false,
+  schedule0: "15:48",
+  schedule1: "00:00",
+  count: 13,
+  waterTemperature: 13.62842,
+};
 
-  let fishFeederData = {
-    "schedule0Enabled": true,
-    "feednow": false,
-    "schedule2Enabled": false,
-    "schedule2": "00:00",
-    "schedule1Enabled": false,
-    "schedule0": "15:48",
-    "schedule1": "00:00",
-    "count": 13,
-    "waterTemperature": 13.62842
-  }
+app.use(apiKeyAuth);
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+app.use(express.json());
 
+const doc = db.collection("fishFeeder");
+
+doc.onSnapshot(
+  (docSnapshot) => {
     // console.log("Collection snapshot received.");
     docSnapshot.forEach((doc) => {
       // console.log(`${doc.id} =>`, doc.data());
