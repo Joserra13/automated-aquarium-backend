c1394beb06bd94f42fbd94f620394d30344e4050
Joserra13 <Joserra_013@hotmail.com>
2025-06-19 18:29:06 +0200

    fix(): Communication with frontend

--- index.js
+++ index.js
@@ -7,52 +7,34 @@ const { db } = require("./firebaseAdmin");
 const app = express();
 const port = 3000;
 
-let data;
+let fishFeederData = {};
 
 app.use(apiKeyAuth)
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 app.use(express.json());
 
-// async function fetchFishFeederData() {
-//   try {
-//     const fishFeederCollection = db.collection("fishFeeder");
-//     const snapshot = await fishFeederCollection.get();
-//     const data = [];
-//     snapshot.forEach((doc) => {
-//       data.push({ id: doc.id, ...doc.data() });
-//     });
-//     return data;
-//   } catch (error) {
-//     console.error("Error fetching fish feeder data:", error);
-//     throw error;
-//   }
-// }
-
 const doc = db.collection('fishFeeder')
 
-const observer = doc.onSnapshot(docSnapshot => {
-  console.log("📦 Collection snapshot received.");
-  docSnapshot.forEach(doc => {
-    data = doc.data();
-    console.log(`${doc.id} =>`, doc.data());
-  });
+doc.onSnapshot(
+  (docSnapshot) => {
+    console.log("Collection snapshot received.");
+    docSnapshot.forEach((doc) => {
+      // console.log(`${doc.id} =>`, doc.data());
+      fishFeederData = doc.data();
 
-}, err => {
-  console.log(`Encountered error: ${err}`);
-});
+      for (let [key, value] of Object.entries(doc.data())) {
+        console.log(`${key}: ${value}`);
+      }
+    });
+  },
+  (err) => {
+    console.log(`Encountered error: ${err}`);
+  }
+);
 
-app.get("/", (req, res) => {
-  // fetchFishFeederData()
-  //   .then((data) => {
-  //     console.log("Fish Feeder Data:", data);
-  //     res.status(200).json(data);
-  //   })
-  //   .catch((error) => {
-  //     console.error("Error:", error);
-  //     res.status(500).json({ error: 'Failed to fetch data' });
-  //   });
-
-  res.status(200).json(data);
+app.get("/fishFeeder", (req, res) => {
+  console.log("GET /fishFeeder");
+  res.json(fishFeederData);
 });
 
 app.listen(port, () => {
--- middleware/apiKeyAuth.js
+++ middleware/apiKeyAuth.js
@@ -1,6 +1,6 @@
 module.exports = function (req, res, next) {
-  const clientKey = req.headers['x-api-key'];
-  if (!clientKey || clientKey !== process.env.API_KEY) {
+  const clientKey = req.headers['api-key'];
+  if (!clientKey || clientKey !== process.env.BACKEND_API_KEY) {
     return res.status(401).json({ error: 'Unauthorized' });
   }
   next();
