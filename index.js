6200f6bd73b6ff382b619ce0b9f01e81403ed148
Joserra13 <Joserra_013@hotmail.com>
2025-07-27 23:47:32 +0200

    fix(): change env var name

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const dotenv = require('dotenv');
 dotenv.config();
 
-const uri = process.env.MONGO_DB;
+const uri = process.env.MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
